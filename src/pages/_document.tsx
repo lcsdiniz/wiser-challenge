78b80e5c6fcec664db6b81830e82b56e868487e2
Lucas Diniz <lucas.henrique@globalsys.com>
2021-06-23 23:10:45 -0300

    Fix: Document typing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,63 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+// export default class MyDocument extends Document {
+//   static getInitialProps({ renderPage }: DocumentContext) {
+//     const sheet = new ServerStyleSheet();
+
+//     const page = renderPage((App) => (props) =>
+//       sheet.collectStyles(<App {...props} />),
+//     );
+
+//     const styleTags = sheet.getStyleElement();
+
+//     return { ...page, styleTags };
+//   }
+
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <link rel="preconnect" href="https://fonts.gstatic.com" />
+//           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet" />
+//           {this.props.styleTags}
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+      
+//     );
+//   }
+// }
+
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />),
-    );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-    const styleTags = sheet.getStyleElement();
-
-    return { ...page, styleTags };
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
@@ -20,7 +66,6 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet" />
-          {this.props.styleTags}
         </Head>
         <body>
           <Main />
